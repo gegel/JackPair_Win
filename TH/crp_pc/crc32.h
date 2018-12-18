45034abd8aece8f31ccd55b2770038237a749071
Van Gegel <torfone@ukr.net>
2018-12-18 09:40:42 +0200

    Add files via upload

---
+++ TH/crp_pc/crc32.h
@@ -0,0 +1,6 @@
+void crc32_init( void );
+void crc32_update( unsigned char *blk_adr, unsigned long blk_len );
+unsigned long crc32_value( void );
+
+unsigned long crc32( unsigned char *blk_adr, unsigned long blk_len );
+unsigned char crc8(const unsigned char * ptr, unsigned length);
